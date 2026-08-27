f0e5f083dead34ac4bc73a889a4e20779ee93766
Abramlouies <abramlouies@outlook.com>
2026-08-27 12:37:38 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/royal_chess/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.royal_chess
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
--- android/gradlew.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET DIR=%~dp0
-IF EXIST "%JAVA_HOME%\bin\java.exe" (SET JAVA_EXE=%JAVA_HOME%\bin\java.exe) ELSE (SET JAVA_EXE=java.exe)
-"%JAVA_EXE%" -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
